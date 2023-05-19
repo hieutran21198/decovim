2f44acb13fe71143c4a477bf7ebaa5b5937962bc
hieu tran <hieu.tran21198@gmail.com>
2023-05-19 17:18:11 +0700

    chore(script): remove unused script

--- scripts/revert.sh
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/env bash
-
-if [ -L "$HOME/.config/nvim" ]; then
-  rm "$HOME/.config/nvim"
-fi
-
-if [ ! -d "$HOME/.config/nvim" ]; then
-  ln -s "$HOME/Workspaces/personal/cirius-nvim" "$HOME/.config/nvim"
-fi
